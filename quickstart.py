11264b3890b7ddaa89826f1f72e45aae66d35a3a
XxCesarinxX <cesar1000sk@gmail.com>
2023-10-02 16:53:53 -0600

    mejorando el search
    aprendimos a filtrar atravez de condiciones boleanas

--- quickstart.py
+++ quickstart.py
@@ -7,7 +7,10 @@ from datetime import datetime
 #*/-------------------credenciales------------------------/'''
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 KEY = r"C:\Users\cesar\OneDrive\Documentos\noEntrar\key.json" 
-SPREADSHEET_ID = '1NiiceNHdvZxlXNjgpC3k_droC_S4Mh5xbS-lMqGktCw'
+SPREADSHEET_ID = {
+    "inventarioGuerrero" :'1NiiceNHdvZxlXNjgpC3k_droC_S4Mh5xbS-lMqGktCw',
+    "ListasDePrecios"    :'1AwlS1NxhRBKn-G_vaiOBmPKW0AVnvQ6DDwb1ZJAYfRA',
+    "HistorialdeCompras" :'1RlDAkYJCeFAq-mMrLwrzshcXxpHeVTj3rvq1EeGqD4E'}
 creds = None
 creds = service_account.Credentials.from_service_account_file(KEY, scopes=SCOPES)
 service = build('sheets', 'v4', credentials=creds)
@@ -26,6 +29,26 @@ dicVentas    = {
     "Precio"  : listPrecio
 }
 
+def leer(hoja,nombreID):
+    result = sheet.values().get(
+        spreadsheetId=SPREADSHEET_ID[nombreID],
+        range= hoja # poner en rango el nombre de la hoja manda toda informacion
+    ).execute()
+    values = result.get('values',[])
+    # Reemplazar los espacios en blanco por nulos
+    values = [[None if x == '' else x for x in row] for row in values]
+    df = pd.DataFrame(values[1:], columns=values[0])
+    
+    columnas_a_convertir = ["S", "M", "L", "Xl", "2Xl", "3Xl", "5Xl"]
+    # Itera a través de las columnas y convierte su tipo de dato a int64
+    for columna in columnas_a_convertir:
+        # Reemplaza los valores None por 0 en las columnas especificadas
+        df[columnas_a_convertir] = df[columnas_a_convertir].fillna(0)
+        # Convierte las columnas al tipo de dato int64
+        df[columna] = df[columna].astype('int64')
+
+    return df
+
 def interaccion():
     n_Ventas= int(input(" vendPrendasidas?:\n-->"))
     for x in range(n_Ventas):
@@ -37,15 +60,17 @@ def interaccion():
             dicVentas[i].append(aux)     
     return dicVentas
 
-def search(inve):
-    dfInvetnario = leer(inve)
+def search():
+    dfInventario = leer("InventarioG" ,"inventarioGuerrero")
     #depende de la libreria pandas
     busqueda = input("Introduce:\n+________+________+\n| Modelo | Talla |\n+--------+--------+\n| ").title()
     busqueda = busqueda.split()
-    respuesta = dfInvetnario.loc[busqueda[0],
-    ("Color", busqueda[1])]
-    print(respuesta)
-    return respuesta
+    print(busqueda)
+    #filtra las filas que cumplen con las condiciones
+    filtro = (dfInventario['Modelo'] == busqueda[0]) & (dfInventario[busqueda[1]] > 0)
+    #una vez filtrado por lo que buscamos solo implementamos los datos que interesan
+    resultado = dfInventario.loc[filtro, ["Modelo", "Color", busqueda[1]]]
+    return resultado
 
 def decrementacion(dFrame, dicci):
     for i in range(len(dicci["Modelo"])):
@@ -59,63 +84,60 @@ def incrementacion(dFrame, dicci):
         aux+=1
         dFrame.loc[(dFrame['Modelo'] == dicci["Modelo"][i]) & (dFrame['Color'] == dicci["Color"][i]), dicci["Talla"][i]]=aux
 
-def leer(hoja):
-    result = sheet.values().get(
-        spreadsheetId=SPREADSHEET_ID,
-        range= hoja # poner en rango el nombre de la hoja manda toda informacion
-    ).execute()
-    values = result.get('values',[])
-    # Reemplazar los espacios en blanco por nulos
-    values = [[None if x == '' else x for x in row] for row in values]
-    df = pd.DataFrame(values[1:], columns=values[0])
-    return df
 
-def subir(hoja,listados):
+def subir(hoja,listados,nombreID):
     sheet.values().update(
-        spreadsheetId=SPREADSHEET_ID,
+        spreadsheetId=SPREADSHEET_ID[nombreID],
         range=hoja,  
         valueInputOption='USER_ENTERED',
         body={'values': listados}
     ).execute()
     
-def subirFila(hoja,fila):
+def subirFila(hoja,fila,nombreID):
     sheet.values().append(
-        spreadsheetId=SPREADSHEET_ID,
+        spreadsheetId=SPREADSHEET_ID[nombreID],
         range=hoja,  
         valueInputOption='USER_ENTERED',
         body={'values': fila}
     ).execute()
 
-def consecuenciaVenta():
-    """------------------------decrementacion-------------------------------"""
-    df = leer("InventarioG")
-    decrementacion(df,dicVentas)
+def listado(dataf):
+    if isinstance(dataf, dict):
+        # Si es un diccionario, conviértelo en un DataFrame
+        df = pd.DataFrame(dataf)
+        dataf = df
+    
+    encabezado =  dataf.columns.tolist()
+    filas      = dataf.values.tolist()
+    listas = [encabezado] + filas
+    return listas
+
+def listaprecios():
+    df=leer("Hoja 1","HistorialdeCompras")
     print(df)
-    """-------------------------solicitar--------------------------------"""
-    dicVentas.pop("Precio")
+
+def listaveticaldic():
     listas=[]
     for i in range(len(listCantidad)):
         aux=[]
         for value in dicVentas.values():
             aux.append(value[i])      
         listas.append(aux)
-    subirFila("SolicitaG",listas)
-        
-def inventario(hoja):
-    df = leer(hoja)
-    incrementacion(df, dicVentas)
-
-def listado(dataf):
-    encabezado,  filas = dataf.columns.tolist(),dataf.values.tolist()
-    listas = [encabezado] + filas
     return listas
-
-def listaprecios():
-    SPREADSHEET_ID="1AwlS1NxhRBKn-G_vaiOBmPKW0AVnvQ6DDwb1ZJAYfRA"
-    df=leer("ListaPre")
+        
+def consecuenciaVenta():
+    """------------------------decrementacion-------------------------------"""
+    df = leer("InventarioG","inventarioGuerrero")
+    decrementacion(df,dicVentas)
     print(df)
+    """------------------------HistorialdeCompras-------------------------------"""
+    
+    """-------------------------solicitar--------------------------------"""
+    dicVentas.pop("Precio")
+    subir("SolicitaG",listado(dicVentas),"inventarioGuerrero")
 
 def pedidos():
+
     peticion  = input("\n--Separado por COMAS--\n| Modelo | Talla | Color | Peticion | Numero | Nombre | Acuenta | Costo |\n")
     peticion  = list(peticion.split(","))
     resultado = {
@@ -131,6 +153,20 @@ def pedidos():
         "Estado"  : "Pedido"
     }
     fila = [] + [list(resultado.values())]
-    subirFila("PedidosG",fila)
+    subirFila("PedidosG",fila,"inventarioGuerrero")
 
-consecuenciaVenta()
+while True:
+    opcion = input("\nElija una opcion\n1.-Anotar Ventas\n2.-Buscar\n3.-Salir\n> ")
+
+    if opcion =="1":
+        consecuenciaVenta()
+
+
+    elif opcion=="2":
+        print(search())
+    
+    elif opcion=="3":
+        print("Saliendo...")
+        break
+    else: 
+        print("XXXXXXXX\tLa opcion que solicitas no es valida\tXXXXXXXX")
